c06015f69e36601d606fe39da9f6193b4ba5d1be
”sojiro0701” <sojiro20060701@gmail.com>
2020-12-06 15:43:15 +0900

    最初に作ったやつ

---
+++ Assets/boru.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class boru : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.GetComponent<Rigidbody>().AddFroce(100, 100, 0);
+
+
+        if (transform.position.y <= -15)
+        {
+            Retry();
+        }
+    }
+    void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
